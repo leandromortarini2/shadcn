3a84e9e3e69c8b51e22b2baac8ba873d7103761e
leandro mortarini <leandromortarini2@gmail.com>
2024-10-28 21:41:21 -0300

    celdas

--- src/app/dashboard/data-table/columns.tsx
+++ src/app/dashboard/data-table/columns.tsx
@@ -1,26 +1,48 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 
 import { Payment } from "@/data/payments.data";
 import { ColumnDef } from "@tanstack/react-table";
-
+import { Badge } from "@/components/ui/badge";
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
 
 export const columns: ColumnDef<Payment>[] = [
   {
+    accessorKey: "clientName",
+    header: "Client Name",
+  },
+  {
     accessorKey: "status",
     header: "Status",
+    cell: ({ row }) => {
+      const status = row.getValue("status") as string;
+      const variant: any =
+        {
+          success: "success",
+          pending: "info",
+          processing: "secondary",
+          failed: "destructive",
+        }[status] ?? ("default" as any);
+      return <Badge variant={variant}>{status}</Badge>;
+    },
+  },
+
+  {
+    accessorKey: "amount",
+    header: () => <div className="text-right">Amount</div>,
+    cell: ({ row }) => {
+      const amount = parseFloat(row.getValue("amount"));
+      const formatted = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(amount);
+
+      return <div className="text-right font-medium">{formatted}</div>;
+    },
   },
   {
     accessorKey: "email",
     header: "Email",
-  },
-  {
-    accessorKey: "amount",
-    header: "Amount",
-  },
-  {
-    accessorKey: "clientName",
-    header: "Client Name",
   },
 ];
