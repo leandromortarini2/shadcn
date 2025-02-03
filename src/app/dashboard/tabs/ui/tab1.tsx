d65d240c632390c30c37e96ee958062365c76f8d
leandro mortarini <leandromortarini2@gmail.com>
2025-02-03 10:47:43 -0300

    correccion tabs

--- src/app/dashboard/tabs/ui/tab1.tsx
+++ src/app/dashboard/tabs/ui/tab1.tsx
@@ -14,23 +14,23 @@ export const Tab1 = () => {
   return (
     <Card>
       <CardHeader>
-        <CardTitle>Password</CardTitle>
+        <CardTitle>Account</CardTitle>
         <CardDescription>
-          Change your password here. After saving, you’ll be logged out.
+          Make changes to your account here. Click save when youre done.
         </CardDescription>
       </CardHeader>
       <CardContent className="space-y-2">
         <div className="space-y-1">
-          <Label htmlFor="current">Current password</Label>
-          <Input id="current" type="password" />
+          <Label htmlFor="name">Name</Label>
+          <Input id="name" defaultValue="Pedro Duarte" />
         </div>
         <div className="space-y-1">
-          <Label htmlFor="new">New password</Label>
-          <Input id="new" type="password" />
+          <Label htmlFor="username">Username</Label>
+          <Input id="username" defaultValue="@peduarte" />
         </div>
       </CardContent>
       <CardFooter>
-        <Button>Save password</Button>
+        <Button>Save changes</Button>
       </CardFooter>
     </Card>
   );
